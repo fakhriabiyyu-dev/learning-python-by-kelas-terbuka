f2d40a46b604a710b95a92f6fdd444f747804bfc
fakhriabiyyu-dev <mfakhriabiyyue.rpl12023@gmail.com>
2026-05-23 16:28:43 +0700

    Menambahkan file materi hari kedua

---
+++ 3_cek.py
@@ -0,0 +1,15 @@
+# cekwaktu_perintah_dijalankan
+import time 
+start_time = time.time()
+
+print('Hello World')
+
+# Ini cara ngecek berapa lama python menjalankan perintah kita
+# kamu bisa cek perbedaan mengeksekusi dengan
+# pyhton namafile.py
+
+# Atau kita bisa mengcomplie python
+# namanya bite code 
+# cara mengvcompile, buka terminal dan tuliskan
+# python -m py_compile Main.py
+print(time.time() - start_time, "detik")
