f358241ce1d34c4e5da4e2f1e336b8bbd33a8b35
fakhriabiyyu-dev <mfakhriabiyyue.rpl12023@gmail.com>
2026-05-26 14:28:32 +0700

    Memasukkan file

---
+++ 5_input_user.py
@@ -0,0 +1,17 @@
+# Episode Input User 
+
+# data yang dimasukkan pasti string 
+data = input("Masukkan data: ")
+
+print("data :", data,"type :", type(data))
+
+# jika kita ingin mengambil int 
+angka = float(input("Masukkan angka: "))
+angka = int(input("Masukkan angka: "))
+
+print("data :", angka, "type : ", type(angka))
+
+# jika inign boolean 
+data_bool = bool(int(input("Masukkan nilai boolean : ")))
+
+print("boolean :", data_bool, "type :", type(data_bool))
