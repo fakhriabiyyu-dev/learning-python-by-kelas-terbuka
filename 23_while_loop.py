babfa57d60b8f60986e32548ffc34c8b1540b13f
fakhriabiyyu-dev <mfakhriabiyyue.rpl12023@gmail.com>
2026-06-30 19:05:46 +0700

    menambahkan file

---
+++ 23_while_loop.py
@@ -0,0 +1,17 @@
+# while loop 
+
+# contohnya 
+# while kondisi:
+#     aksi ini 
+#     aksi itu
+
+# akhir dari program
+
+angka = 0
+print(f"angka sekarang -> {angka}")
+
+while angka < 5:
+    angka += 1
+
+    print(f"abiyyu kelas bang! btw ini ke- {angka}")
+print("cukup, kamu terlalu bersinar")
