7d93e556a2826590d4f377788a6332996f866040
fakhriabiyyu-dev <mfakhriabiyyue.rpl12023@gmail.com>
2026-07-08 20:45:29 +0700

    menambahkan file

---
+++ 22_for_loop.py
@@ -0,0 +1,78 @@
+# Perulangan (loop)
+
+# contohnya
+# for kondisi:
+#     aksi
+
+# for(int i=0; i <= 10; i++) {
+#     print{i}
+# }
+
+#
+#
+#
+#
+#
+
+# contoh konsep lainnya menggunakan list 
+# angka2 = [0,1,6,4,9,10] 
+# print(angka2)
+
+# # walau listnya acak dia tetap mengurutkan dari sebelah kiri
+# for i in angka2:
+#     print(f"i sekarang -> {i}")
+# print("akhir dari program 1")
+
+#
+#
+#
+#
+#
+
+# note :  in (didalam)
+        # range (jarak atau selisih)
+#                 (yang gw pahamin range adalah acuan seberapa banyak.)
+# contoh pertama 
+# for i in range(5):
+#     print(f"i sekarang -> {i}")
+# print("akhir dari program 1.\n")
+
+#
+#
+#
+#
+#
+
+# contoh range kedua 
+# angka2_range = range(7)
+
+# for i in angka2_range:
+#     print(f"i sekarang -> {i}")
+# print("akhir dari program 2.\n")
+
+#
+#
+#
+#
+#
+
+# contoh range ketiga 
+# # koma disitu adalah pembatas
+# angka3_range = range(1,10)
+
+# for i in angka3_range:
+#     print(f"i sekarang -> {i}")
+# print("akhir dari program 3.\n")
+
+#
+#
+#
+#
+#
+
+# contoh bonus
+data_str = "saya ganteng abis"
+
+for huruf in data_str:
+    print(huruf)
+print("akhir dari program 4.\n")
